d5d7c1073148f9f4e26775a5e467bcf770b2f24b
Prafulrakhade <prafulrakhade02@gmail.com>
2026-03-12 13:39:20 +0530

    [MOSIP-44618] removed artifactory from certify deployment script
    Signed-off-by: Prafulrakhade <prafulrakhade02@gmail.com>

--- deploy/inji-certify/install.sh
+++ deploy/inji-certify/install.sh
@@ -32,7 +32,6 @@ function installing_inji-certify() {
   echo "Copy configmaps"
   COPY_UTIL=../copy_cm_func.sh
   $COPY_UTIL configmap artifactory-share artifactory $NS
-  $COPY_UTIL configmap config-server-share config-server $NS
   $COPY_UTIL configmap softhsm-certify-share softhsm $NS
 
   echo "Copy secrets"
